d6d59a795c2a83a95f966bb1d62f7139865e53cb
Enigmify <69073505+TERIHAX@users.noreply.github.com>
2023-08-27 20:18:21 +0100

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://github.com/TERIRepoBackups/SimpleSpySource/raw/master/SimpleSpyIntro.lua', true))()
+
+loadstring(game:HttpGet('https://github.com/TERIRepoBackups/SimpleSpySource/raw/master/SimpleSpy.lua', true))()
